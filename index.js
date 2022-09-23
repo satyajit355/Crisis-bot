b29655950535d37aac84c49602d3adef410cf630
Satyajit Op <73672937+satyajit355@users.noreply.github.com>
2022-09-23 09:20:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({ intents: 7753 });
+const fs = require("fs");
+const Prefix = "8383838";
+
+
+fs.readdir("./events/discord", (_err, files) => {
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    const event = require(`./events/discord/${file}`);
+    let eventName = file.split(".")[0];
+    console.log(`[Event]   ✅  Loaded: ${eventName}`);
+    client.on(eventName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/discord/${file}`)];
+  });
+});
+
+/* Load all events (giveaways based) */
+
+
+fs.readdir("./events/giveaways", (_err, files) => {
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    const event = require(`./events/giveaways/${file}`);
+    let eventName = file.split(".")[0];
+    console.log(`[Event]   🎉 Loaded: ${eventName}`);
+    client.giveawaysManager.on(eventName, (...file) => event.execute(...file, client)), delete require.cache[require.resolve(`./events/giveaways/${file}`)];
+  })
+})
+
+// Let commands be a new collection ( message commands )
+client.commands = new Discord.Collection();
+/* Load all commands */
+fs.readdir("./commands/", (_err, files) => {
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, {
+      name: commandName,
+      ...props
+    });
+    console.log(`[Command] ✅  Loaded: ${commandName}`);
+  });
+});
+
+
+// let interactions be a new collection ( slash commands  )
+client.interactions = new Discord.Collection();
+// creating an empty array for registering slash commands
+client.register_arr = []
+/* Load all slash commands */
+fs.readdir("./slash/", (_err, files) => {
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./slash/${file}`);
+    let commandName = file.split(".")[0];
+    client.interactions.set(commandName, {
+      name: commandName,
+      ...props
+    });
+    client.register_arr.push(props)
+  });
+});
+
+
+// Login through the client
+client.login("ODczODEwMjUzNjkzNzE0NDgy.GhYR56.65sGWCEUdY_4nceNcEkhDq3_CBzwT6cXmLLMrQ");
